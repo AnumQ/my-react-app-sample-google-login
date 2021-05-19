bb5f2fc7dd6f3bf632dc1bfa0cc3ca4861bcab69
Anum Qudsia <hello@anum.io>
2021-05-19 13:50:30 +0200

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { GoogleLoginButton } from "./Components/UI/GoogleLoginButton";
-import { auth } from "./firebase";
-import Box from "@material-ui/core/Box";
-import LinearProgress from "@material-ui/core/LinearProgress";
 import { useAuthUser } from "./hooks/useAuthUser";
-import { Flex, Container, CenterContainer, Menu } from "./Components/UI/shared";
-import { LogoutButton } from "./Components/UI/LogoutButton";
+import { Container } from "./Components/UI/shared";
+import { LoadingView } from "./Components/UI/LoadingView";
+import { LoginView } from "./Login/LoginView";
+import { Main } from "./Main/Main";
 
 function App() {
   const { authUser, isLoading } = useAuthUser();
 
-  const projectTitle = "my-react-app-sample-google-login";
-
   return (
     <Container>
-      {isLoading ? (
-        <Flex fullHeight>
-          <Box style={{ width: "100%" }}>
-            <LinearProgress color="secondary" />
-          </Box>
-        </Flex>
-      ) : !authUser ? (
-        <Flex fullHeight>
-          <CenterContainer>
-            <div
-              style={{
-                height: "5rem",
-                textAlign: "center",
-              }}
-            >
-              {projectTitle}
-            </div>
-            <GoogleLoginButton />
-          </CenterContainer>
-        </Flex>
-      ) : (
-        <Menu>
-          <div>{projectTitle}</div>
-          <LogoutButton
-            handleLogout={() => {
-              auth.signOut();
-            }}
-          />
-        </Menu>
-      )}
+      {isLoading ? <LoadingView /> : !authUser ? <LoginView /> : <Main />}
     </Container>
   );
 }
